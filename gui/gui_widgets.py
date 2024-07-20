2a2e5de69c0da91e5895465162e3807ef50056ad
franciscolou <franciscogardenberg@gmail.com>
2024-07-20 04:20:12 -0300

    Final adjustments V1 + widget modularization

---
+++ gui/gui_widgets.py
@@ -0,0 +1,42 @@
+from PyQt6 import QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtSvgWidgets import QSvgWidget
+
+class SearchWidget(QtWidgets.QFrame):
+    def __init__(self, ui, parent, x, y, width, height, object_name, background_color, search_bar_width, items_type):
+        super().__init__(parent)
+        self.setGeometry(x, y, width, height)
+        self.setObjectName(object_name)
+        self.setStyleSheet(f'background-color: {background_color}')
+        self.show()
+        self.searchBar = QtWidgets.QLineEdit(self)
+        self.searchBar.setObjectName("searchBar")
+        searchBarSpace = search_bar_width + 10
+        self.searchBar.setGeometry(10, int(height*0.2), search_bar_width, int(height*0.6))
+        self.searchBar.setStyleSheet("background-color: #FFFFFF; color: #000000; padding-left: 3px;")
+        ui.current_font.setPointSize(12)
+        ui.current_font.setBold(False)
+        self.searchBar.setFont(ui.current_font)
+        self.searchBar.installEventFilter(ui)
+        self.searchBar.setPlaceholderText("Buscar...")
+        self.searchBar.show()
+
+        searchNavUtilsY = self.size().height()//2 - 10
+        self.resultsTracker = QtWidgets.QLabel(self)
+        self.resultsTracker.setObjectName("resultsTracker")
+        self.resultsTracker.setText("---")
+        self.resultsTracker.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.resultsTracker.setFixedSize(60, int(height*0.6))
+        self.resultsTracker.move(searchBarSpace + 5, height//2 - self.resultsTracker.size().height()//2)
+
+        self.backButton = QSvgWidget("gui/assets/images/up_arrow_disabled.svg", self)
+        self.backButton.setObjectName("backButton")
+        self.backButton.setGeometry(searchBarSpace + 60 + 10, searchNavUtilsY, 20, 20)
+        self.backButton.mousePressEvent = lambda event: ui.highlight_selected_result(-1, items_type)
+        self.backButton.show()
+
+        self.nextButton = QSvgWidget("gui/assets/images/down_arrow_disabled.svg", self)
+        self.nextButton.setObjectName("nextButton")
+        self.nextButton.setGeometry(searchBarSpace + 5 + 60 + 5 + 20 + 5, searchNavUtilsY, 20, 20)
+        self.nextButton.mousePressEvent = lambda event: ui.highlight_selected_result(1, items_type)
+        self.nextButton.show()
