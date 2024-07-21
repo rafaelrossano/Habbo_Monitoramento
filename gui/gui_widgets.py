64ff28ecf5cf5ebd425c095b0f54a9661f48b3f7
franciscolou <franciscogardenberg@gmail.com>
2024-07-21 14:09:57 -0300

    Bugfixes, hover effects and filter feature

--- gui/gui_widgets.py
+++ gui/gui_widgets.py
@@ -2,8 +2,9 @@ from PyQt6 import QtWidgets
 from PyQt6.QtCore import Qt
 from PyQt6.QtSvgWidgets import QSvgWidget
 
+containerColor = "#474747"
 class SearchWidget(QtWidgets.QFrame):
-    def __init__(self, ui, parent, x, y, width, height, object_name, background_color, search_bar_width, items_type):
+    def __init__(self, ui, parent, x, y, width, height, object_name, background_color, search_bar_width, items_type, results_tracker_text):
         super().__init__(parent)
         self.setGeometry(x, y, width, height)
         self.setObjectName(object_name)
@@ -21,13 +22,13 @@ class SearchWidget(QtWidgets.QFrame):
         self.searchBar.setPlaceholderText("Buscar...")
         self.searchBar.show()
 
-        searchNavUtilsY = self.size().height()//2 - 10
+        searchNavUtilsY = int(self.size().height()/2 - 10)
         self.resultsTracker = QtWidgets.QLabel(self)
         self.resultsTracker.setObjectName("resultsTracker")
-        self.resultsTracker.setText("---")
+        self.resultsTracker.setText(results_tracker_text)
         self.resultsTracker.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
         self.resultsTracker.setFixedSize(60, int(height*0.6))
-        self.resultsTracker.move(searchBarSpace + 5, height//2 - self.resultsTracker.size().height()//2)
+        self.resultsTracker.move(searchBarSpace + 5, int(height/2 - self.resultsTracker.size().height()/2))
 
         self.backButton = QSvgWidget("gui/assets/images/up_arrow_disabled.svg", self)
         self.backButton.setObjectName("backButton")
@@ -40,3 +41,11 @@ class SearchWidget(QtWidgets.QFrame):
         self.nextButton.setGeometry(searchBarSpace + 5 + 60 + 5 + 20 + 5, searchNavUtilsY, 20, 20)
         self.nextButton.mousePressEvent = lambda event: ui.highlight_selected_result(1, items_type)
         self.nextButton.show()
+
+        print("quantidade de elementos na lista:", len(self.findChildren(QSvgWidget)))
+        for widget in self.findChildren(QSvgWidget):
+            widget.setStyleSheet("background-color: " + background_color + ";")
+            widget.setCursor(Qt.CursorShape.PointingHandCursor)
+            widget.installEventFilter(ui)
+            widget.enterEvent = lambda event, w=widget: w.setStyleSheet("border-radius: 5px; background-color: " + containerColor + ";")
+            widget.leaveEvent = lambda event, w=widget: w.setStyleSheet("background-color: " + background_color + ";")
